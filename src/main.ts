c7185e73abcfafadde561ff8a6374c739656395b
Edwin Martin <edwin@bitstorm.org>
2024-10-26 22:08:40 +0200

    Add interaction

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+console.log('Hello World [1]');
+
+async function hello() {
+    const [tab] = await chrome.tabs.query({active: true});
+
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id!},
+        func: () => {
+            alert(`Title is: ${document.title}`);
+        }
+    })
+}
+
+document.getElementById('out')!.textContent = new Date().toLocaleTimeString();
+
+document.getElementById('start')!.addEventListener('click', hello);
